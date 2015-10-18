ed5f430868171806e7bb0cfe948f319a9f201aae
Torsten Werner <torsten.werner@westernacher.com>
2015-10-18 21:43:39 +0200

    add gw2 api links as comments

--- gw2.js
+++ gw2.js
@@ -13,3 +13,9 @@ angular.module('gw2', []).
             localStorage.setItem("language", $scope.lang);
         };
     });
+
+// https://wiki.guildwars2.com/wiki/API:Main
+// https://api.guildwars2.com/v2/worlds?ids=all&lang=de
+// https://api.guildwars2.com/v2/wvw/matches/2-1?lang=de
+// https://api.guildwars2.com/v1/wvw/objective_names.json?lang=de
+// https://gist.github.com/torstenwerner/2bae98192f8fbd608a85
